9ddea33515d071510566d647cda21f860b861c60
thoratatul007 <devpoo91@gmail.com>
2020-06-08 00:20:57 -0700

    Added Test Project2

---
+++ TestProject2/src/com/testP/MyTestProject.java
@@ -0,0 +1,13 @@
+package com.testP;
+
+public class MyTestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("This is Test");
+
+	}
+
+}
