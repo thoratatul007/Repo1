2c8220f021ade4e6014e42a6a26576e4ead0560f
thoratatul007 <devpoo91@gmail.com>
2020-06-08 21:16:20 -0700

    adde amven Project

---
+++ DemoTools/src/test/java/MyTestMaven.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class MyTestMaven {
+	
+	@Test
+	public void myTest()
+	
+	{
+		
+		for(int i=1;i<=10;i++)
+		{
+			
+			System.out.println("Hello World "+i);
+		}
+	}
+
+}
