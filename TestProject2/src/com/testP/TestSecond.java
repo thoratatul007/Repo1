fd6722805d12f882b31fee8caed7537464e4ea37
thoratatul007 <devpoo91@gmail.com>
2020-06-08 19:10:38 -0700

    Updated testProject2

--- TestProject2/src/com/testP/TestSecond.java
+++ TestProject2/src/com/testP/TestSecond.java
@@ -1,5 +1,20 @@
 package com.testP;
 
+import org.testng.annotations.Test;
+
 public class TestSecond {
+	
+	@Test
+	public void myTest()
+	
+	{
+		
+		for(int i=1;i<=10;i++)
+		{
+			
+			System.out.println("Hello World "+i);
+		}
+	}
+			
 
 }
